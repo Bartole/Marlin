28c505ddf662f0781abacf2e14a766ca567e05fc
bartole <bart@bvtol.eu>
2020-07-09 17:33:23 +0200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -73,7 +73,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Bart van Tol, 20200703)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Bart van Tol, 20200709)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
