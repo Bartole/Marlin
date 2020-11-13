bd2d09fd86ef0e3d8f7170dfb5f8baedffce1757
bartole <bart@bvtol.eu>
2020-11-13 15:02:09 +0100

    Update Version.h

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -41,12 +41,12 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-#define STRING_DISTRIBUTION_DATE "2020-11-12"
+#define STRING_DISTRIBUTION_DATE "2020-11-13"
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
  */
-#define MACHINE_NAME "Ender-3 Pro"
+#define MACHINE_NAME "E3Pro Henk"
 
 /**
  * The SOURCE_CODE_URL is the location where users will find the Marlin Source
