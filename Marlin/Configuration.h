08e509eba0682d8bd1b43805c38dbfbb03c852c2
bartole <bart@bvtol.eu>
2020-11-16 20:01:25 +0100

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -134,7 +134,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "E3Pro Henk"
+#define CUSTOM_MACHINE_NAME "E3-Pro Henk"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
